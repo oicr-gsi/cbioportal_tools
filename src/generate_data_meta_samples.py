1fde9a40efa2f0888571f68a7bd3f15070b48521
Kunal Chandan <kunal_chandan@outlook.com>
2019-01-30 16:55:04 -0500

    Refactor for ease, bug fix, update README

--- src/generate_data_meta_samples.py
+++ src/generate_data_meta_samples.py
@@ -19,18 +19,18 @@ def define_parser():
                                                  "pipeline, and put them into the correct cBioPortal import files "
                                                  "(https://cbioportal.readthedocs.io/en/latest/File-Formats.html).")
     required = parser.add_argument_group('Required Arguments')
-    required.add_argument("-f", "--study-input-folder",
+    required.add_argument("-i", "--study-input-folder",
                           type=lambda folder: helper.check_files_in_folder(helper.extensionChoices, folder, parser),
                           help="The input folder can contain compressed: [" +
                                " | ".join(helper.compressedChoices) + "] "
                                                                       " or uncompressed format in: [" +
                                " | ".join(helper.extensionChoices) + "] ",
                           metavar='')
-    required.add_argument("-i", "--study-id",
+    required.add_argument("-s", "--study-id",
                           help="This is the cancer study ID, a unique string. Please use the format gene_lab_year. e.g."
                                "brca_gsi_2019 or mixed_tgl_2020",
                           metavar='')
-    required.add_argument("-s", "--study-folder",
+    required.add_argument("-o", "--study-output-folder",
                           help="The folder you want to export this generated data_samples.txt file to. Generally this "
                                "will be the main folder of the study being generated. If left blank this will generate "
                                "it wherever you run the script from.",
