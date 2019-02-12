adeb1e3c17fffeeaf93e31f2acae589302c6b694
Kunal Chandan <kunal_chandan@outlook.com>
2019-02-12 14:54:57 -0500

    Clean output folder before generating

--- src/helper.py
+++ src/helper.py
@@ -4,6 +4,7 @@ __email__ = "kchandan@uwaterloo.ca"
 __status__ = "Pre-Production"
 
 import os
+import shutil
 
 import numpy as np
 import re
@@ -44,6 +45,17 @@ def make_folder(path):
         os.stat(path)
     except OSError:
         os.mkdir(path)
+
+
+def clean_folder(path):
+    for the_file in os.listdir(path):
+        file_path = os.path.join(path, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path): shutil.rmtree(file_path)
+        except Exception as e:
+            print(e)
 
 
 def reset_folder(owd):
