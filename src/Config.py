2064c84e0be8c72cbf4b45d98a5d57ee086058da
Kunal Chandan <kunal_chandan@outlook.com>
2019-02-21 17:38:43 -0500

    Bug Fixes, Add Clinical Data, Tested, Fixed Examples

--- src/Config.py
+++ src/Config.py
@@ -1,15 +1,28 @@
 import pandas as pd
 
 
-class Config:
-    config_map: map = {}
+class Config(object):
+    config_map: dict = {}
     data_frame: pd.DataFrame = pd.DataFrame()
     type_config: str = ''
 
-    def __init__(self, config_map, data_frame, type_config):
-        self.config_map = config_map
-        self.data_frame = data_frame
+    def __init__(self, config_map: dict, data_frame: pd.DataFrame, type_config: str):
+        self.config_map = config_map.copy()
+        self.data_frame = data_frame.copy()
         self.type_config = type_config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(config.config_map, config.data_frame, config.type_config)
 
     def __str__(self):
         return str([self.config_map, self.data_frame, self.type_config])
+
+
+class Clinical_Config(Config):
+    data_frame: list
+
+    def __init__(self, config_map: dict, array, type_config: str):
+        self.config_map = config_map
+        self.data_frame = array
+        self.type_config = type_config
