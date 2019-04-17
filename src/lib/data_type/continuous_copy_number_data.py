8e742358891968b3e9115cfd43c9c127c8d025be
Kunal Chandan <kunal_chandan@outlook.com>
2019-04-17 15:23:38 -0400

    Validation of 'FILE' import, Resolve CNA issue, Add retreival of SAMPLE_IDs from 'FILE'

--- src/lib/data_type/continuous_copy_number_data.py
+++ src/lib/data_type/continuous_copy_number_data.py
@@ -21,7 +21,22 @@ def gen_log2cna(exports_config: Config.Config, study_config: Config.Config, janu
     helper.working_on(verb, message='Generating log2CNA...')
 
     # This may break if after loading the module R-gsi/3.5.1, Rscript is not set as a constant
-    helper.call_shell('Rscript {}'
+    helper.call_shell('Rscript {} '
                       '-s {} '
                       '-g {} '
-                      '-o {} '.format(os.path.join(janus_path, 'src/lib/data_type/seg2gene.R '), seg_file, bed_file, l_o_file), verb)
+                      '-o {} '.format(os.path.join(janus_path, constants.seg2gene), seg_file, bed_file, l_o_file), verb)
+
+
+def verify_final_file(exports_config: Config.Config, verb):
+    data = open(os.path.join(exports_config.config_map['input_folder'],
+                            exports_config.data_frame['FILE_NAME'][0]), 'w')
+
+    t_config = exports_config.type_config
+    header = data.readline().strip().split('\t')
+    minimum_header = ['Entrez_Gene_Id', 'Hugo_Symbol']
+
+    helper.working_on(verb, message='Asserting minimum header is in {} file.'.format(t_config))
+    if not any([a in header for a in minimum_header]):
+        print([a if a not in header else '' for a in minimum_header])
+        print('Missing header(s) from {} file have been printed above, ensure data isn\'t missing.'.format(t_config))
+        exit(1)
