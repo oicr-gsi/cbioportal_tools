a0fc8f4de6b28fca202adf0253f2539eb9d4f336
Kunal Chandan <kunal_chandan@outlook.com>
2019-04-27 22:15:28 -0400

    Retrieve Authentication from portal.properties

--- src/lib/tools/query.py
+++ src/lib/tools/query.py
@@ -7,6 +7,7 @@ import argparse
 
 from ..support.helper import stars, working_on, get_shell
 
+
 def define_parser() -> argparse.ArgumentParser:
     # Define program arguments
     parser = argparse.ArgumentParser(description="cBioPortal SQL Query Script.")
@@ -16,10 +17,10 @@ def define_parser() -> argparse.ArgumentParser:
                         help="The location of the cBioPortal instance (address).",
                         metavar='URL',
                         required=True)
-    parser.add_argument("-p", "--password",
-                        help="mySQL Password.",
-                        metavar='PASSWORD',
-                        required=True)
+    parser.add_argument("-p", "--properties",
+                        help="portal.properties file path. Default ~/cbioportal/portal.properties",
+                        metavar='PATH',
+                        default='~/cbioportal/portal.properties')
     parser.add_argument("-k", "--key",
                         help="The location of the cBioPortal Key.",
                         metavar='KEY',
@@ -40,7 +41,26 @@ def define_parser() -> argparse.ArgumentParser:
     return parser
 
 
-def query_portal(key: str, cbioportal_url: str, password: str, query_toc: bool, query_gp: bool, border, verb):
+def retrieve_auth(key, url, properties_path, verb) -> [str, str]:
+    if not key == '':
+        key = '-i ' + key
+
+    stars()
+    out = get_shell("ssh {} debian@{} '"
+                    "grep -E 'db.user|db.password' {}'".format(key,
+                                                               url,
+                                                               properties_path), verb)
+    stars()
+    print(out)
+    stars()
+    [user, password] = out.strip().splitlines(keepends=False)
+    user = user.replace('db.user=', '')
+    password = password.replace('db.password=', '')
+
+    return [user, password]
+
+
+def query_portal(key: str, cbioportal_url: str, user: str, password: str, query_toc: bool, query_gp: bool, border, verb):
     if not key == '':
         key = '-i ' + key
 
@@ -51,11 +71,12 @@ def query_portal(key: str, cbioportal_url: str, password: str, query_toc: bool, 
 
     if query_toc:
         stars()
-        out = get_shell("ssh {} debian@{} 'mysql -u root -p{} {} -e "
+        out = get_shell("ssh {} debian@{} 'mysql -u {} -p{} {} -e "
                         "\"use cbioportal; "
                         "select * "
                         "from type_of_cancer;\"'".format(key,
                                                          cbioportal_url,
+                                                         user,
                                                          password,
                                                          border), verb)
         print('TYPE OF CANCER with keywords, name, descriptions, e.t.c. in the cBioPortal mysql database')
@@ -70,6 +91,7 @@ def query_portal(key: str, cbioportal_url: str, password: str, query_toc: bool, 
                         "select STABLE_ID, DESCRIPTION "
                         "from gene_panel;\"'".format(key,
                                                      cbioportal_url,
+                                                     user,
                                                      password,
                                                      border), verb)
         print('GENE PANELS with descriptions in the cBioPortal mysql database')
@@ -81,7 +103,8 @@ def query_portal(key: str, cbioportal_url: str, password: str, query_toc: bool, 
 
 
 def main(args):
+    [user, password] = retrieve_auth(args.key, args.url, True)
     if args.gene_panel or args.type_of_cancer:
-        query_portal(args.key, args.url, args.password, args.type_of_cancer, args.gene_panel, args.border, True)
+        query_portal(args.key, args.url, user, password, args.type_of_cancer, args.gene_panel, args.border, True)
     else:
         print('ERROR:: Arguments -g/--gene-panel and/or -t/--type-of-cancer are required.')
