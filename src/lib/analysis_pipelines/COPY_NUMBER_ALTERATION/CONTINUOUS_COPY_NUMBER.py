c43e7c1867cbd23cccd9faa6643d59a9a4c71651
lheisler <lheisler.bioinformatics@gmail.com>
2019-05-10 13:20:41 -0400

    modified COPY_NUMBER_ALTERATION pipeline scripts to conform to reorganization.

--- src/lib/analysis_pipelines/COPY_NUMBER_ALTERATION/CONTINUOUS_COPY_NUMBER.py
+++ src/lib/analysis_pipelines/COPY_NUMBER_ALTERATION/CONTINUOUS_COPY_NUMBER.py
@@ -1,6 +1,5 @@
 from lib.support import helper
-from lib.data_type.DISCRETE_COPY_NUMBER import discrete_copy_number_data
-
+from lib.analysis_pipelines.COPY_NUMBER_ALTERATION.support_functions import discrete_copy_number_data
 
 def main():
     global meta_config
