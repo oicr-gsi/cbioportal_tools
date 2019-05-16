c55d9de10e8b15601978aff09728067a0bbd3b48
lheisler <lheisler.bioinformatics@gmail.com>
2019-05-16 15:32:50 -0400

    ##29 changes to multiple scripts to correct inconsistencies on how data is referenced. ALTERATIONTYPE:DATATYPE is now a general unique value used as a key for both validation of information and decisions making on how to process. ALL test pipelines appear to be working, will merge this to the master and extend development further on the dev branch

--- src/lib/analysis_pipelines/MRNA_EXPRESSION/support_functions.py
+++ src/lib/analysis_pipelines/MRNA_EXPRESSION/support_functions.py
@@ -37,7 +37,7 @@ def alpha_sort(exports_config: Config.Config, verb):
 
 def generate_expression_matrix(exports_config: Config.Config, study_config: Config.Config, verb):
     output_file = os.path.join(study_config.config_map['output_folder'],
-                               'data_{}.txt'.format(config2name_map[exports_config.type_config]))
+                               'data_{}.txt'.format(config2name_map[exports_config.alterationtype + ":" + exports_config.datatype]))
 
     helper.working_on(verb, message='Reading FPKM data ...')
     info: DataFrames = []
@@ -64,3 +64,28 @@ def generate_expression_matrix(exports_config: Config.Config, study_config: Conf
 
     helper.working_on(verb, message='Writing all FPKM data ...')
     result.to_csv(output_file, sep='\t', index=None)
+
+
+def generate_expression_zscore(exports_config: Config.Config, study_config: Config.Config, verb):
+    input_file = os.path.join(study_config.config_map['output_folder'],
+                              'data_{}.txt'.format(config2name_map[exports_config.alterationtype + ":" + exports_config.datatype]))
+
+    output_file = os.path.join(study_config.config_map['output_folder'],
+                               'data_{}.txt'.format(config2name_map[exports_config.alterationtype + ":Z-SCORE"]))
+
+    # Z-Scores written by Dr. L Heisler
+    helper.working_on(verb, message='Reading FPKM Matrix ...')
+    raw_data = pd.read_csv(input_file, sep='\t')
+
+    helper.working_on(verb, message='Processing FPKM Matrix ...')
+    raw_scores = raw_data.drop(['Hugo_Symbol'], axis=1)
+    means = raw_scores.mean(axis=1)
+    sds = raw_scores.std(axis=1)
+
+    z_scores = ((raw_scores.transpose() - means) / sds).transpose()
+    z_scores = z_scores.fillna(0)
+    z_scores = z_scores.round(decimals=4)
+    z_scores_data = pd.concat([raw_data['Hugo_Symbol'], z_scores], axis=1)
+
+    helper.working_on(verb, message='Writing FPKM Z-Scores Matrix ...')
+    z_scores_data.to_csv(output_file, sep="\t", index=False)
