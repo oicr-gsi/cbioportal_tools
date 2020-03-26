adeccfb9ecfd178dd9c21973ac3d7f6fee28f7da
Allan Liang <aliang@oicr.on.ca>
2020-03-26 18:22:57 -0400

    Included generation of cases_3way_complete

--- src/lib/study_generation/case.py
+++ src/lib/study_generation/case.py
@@ -38,3 +38,36 @@ def generate_case_list(meta_config: Config.Config, study_config: Config.Config, 
         f.write('case_list_ids: {}\n'.format('\t'.join(ids)))
         f.flush()
         f.close()
+
+        # Generate cnaseq case list if there is already cases_cna (CNA data) and cases_sequenced (mutation data)
+        if os.path.isfile(os.path.join(case_list_folder, 'cases_cna.txt') and os.path.isfile(os.path.join(case_list_folder, 'cases_sequenced.txt')\
+                and os.path.isfile(os.path.join(case_list_folder, 'cases_cnaseq.txt') == False:
+            f = open(os.path.join(case_list_folder, 'cases_cnaseq.txt'.format(suffix)), 'w')
+            f.write('cancer_study_identifier: {}\n'.format(study_config.config_map['cancer_study_identifier']))
+            f.write('stable_id: {}{}\n'.format(study_config.config_map['cancer_study_identifier'], '_cnaseq'))        
+            f.write('case_list_name: {}\n'.format('Samples profiled for cnas and sequencing'))
+            f.write('case_list_description: {}\n'.format('This is this case list that contains all samples that are profiled for mutations and cnas.'))
+            try:
+                ids = meta_config.data_frame['SAMPLE_ID']
+            except KeyError:
+                # If pipeline is FILE, this will probably happen
+                ids = data.get_sample_ids(meta_config, verb)
+            f.write('case_list_ids: {}\n'.format('\t'.join(ids)))
+            f.flush()
+            f.close()
+
+        if os.path.isfile(os.path.join(case_list_folder, 'cases_cna.txt') and os.path.isfile(os.path.join(case_list_folder, 'cases_sequenced.txt')\
+                and os.path.isfile(os.path.join(case_list_folder, 'cases_rna_seq_mrna.txt') and os.path.isfile(os.path.join(case_list_folder, 'cases__3way_complete.txt') == False:
+            f = open(os.path.join(case_list_folder, 'cases__3way_complete.txt'.format(suffix)), 'w')
+            f.write('cancer_study_identifier: {}\n'.format(study_config.config_map['cancer_study_identifier']))
+            f.write('stable_id: {}{}\n'.format(study_config.config_map['cancer_study_identifier'], '_3way_complete'))
+            f.write('case_list_name: {}\n'.format'Samples profiled for all of mutation, cnas, and rnaseq'))
+            f.write('case_list_description: {}\n'.format('This is this case list that contains all samples that are profiled for mutations, cnas, and rnaseq.'))
+            try:
+                ids = meta_config.data_frame['SAMPLE_ID']
+            except KeyError:
+                # If pipeline is FILE, this will probably happen
+                ids = data.get_sample_ids(meta_config, verb)
+            f.write('case_list_ids: {}\n'.format('\t'.join(ids)))
+            f.flush()
+            f.close()
