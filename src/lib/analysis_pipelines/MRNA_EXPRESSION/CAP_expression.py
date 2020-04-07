1fdb31fdc7f6268631ed11682d858d9b733518ba
Allan Liang <aliang@oicr.on.ca>
2020-04-07 17:43:53 -0400

    Add functionality to generate tcga and comparison data (needs more work). Changed how expression matrix, zscores, and percentile data are generated

--- src/lib/analysis_pipelines/MRNA_EXPRESSION/CAP_expression.py
+++ src/lib/analysis_pipelines/MRNA_EXPRESSION/CAP_expression.py
@@ -1,6 +1,11 @@
+import os
+
 from lib.support import helper
 from lib.study_generation import meta
-from lib.analysis_pipelines.MRNA_EXPRESSION.support_functions import alpha_sort, generate_expression_matrix, generate_expression_zscore, get_metadata, preProcRNA
+from lib.analysis_pipelines.MRNA_EXPRESSION.support_functions import alpha_sort, generate_expression_matrix, generate_expression_zscore, get_metadata, preProcRNA, generate_TCGA_data
+#TEST#
+from lib.analysis_pipelines.MRNA_EXPRESSION.compData import convertList
+from lib.constants.constants import config2name_map
 
 def main():
     global meta_config
@@ -21,21 +26,62 @@ def main():
     helper.working_on(verb)
 
     # Works because shorting ...
-    if 'zscores' in meta_config.config_map.keys() and meta_config.config_map['zscores'].lower() == 'true':
-        helper.working_on(verb, message='Generating expression Z-Score Meta ...')
-        meta.generate_meta_type(meta_config,study_config,verb)
-        #meta.generate_meta_type(meta_config.alterationtype + '_ZSCORES',
-        #                {'profile_name': 'mRNA expression z-scores','profile_description': 'Expression level z-scores'}, study_config, verb)
-        helper.working_on(verb)
+    #if 'zscores' in meta_config.config_map.keys() and meta_config.config_map['zscores'].lower() == 'true':
+    #    helper.working_on(verb, message='Generating expression Z-Score Meta ...')
+    #    meta.generate_meta_type(meta_config,study_config,verb)
+    #    #meta.generate_meta_type(meta_config.alterationtype + '_ZSCORES',
+    #    #                {'profile_name': 'mRNA expression z-scores','profile_description': 'Expression level z-scores'}, study_config, verb)
+    #    helper.working_on(verb)
 
-        helper.working_on(verb, message='Generating expression Z-Score Data ...')
-        generate_expression_zscore(meta_config, study_config, verb)
-        helper.working_on(verb)
+    #    helper.working_on(verb, message='Generating expression Z-Score Data ...')
+    #    generate_expression_zscore(os.path.join(study_config.config_map['output_folder'],
+    #        'data_{}.txt'.format(config2name_map[meta_config.alterationtype + ":" + meta_config.datahandler])), 
+    #        os.path.join(study_config.config_map['output_folder'], 
+    #            'data_{}.txt'.format(config2name_map[meta_config.alterationtype + ":Z-SCORE"]))
+    #        , verb)
+    #    helper.working_on(verb)
 
     #get meta_expression.txt and meta_expression_zscores.txt
     get_metadata(meta_config, study_config)
     
-    preProcRNA(meta_config, study_config, meta_config.config_map['enscon'], meta_config.config_map['genelist'])
+    # change the gepcomp list
+    #convertList(meta_config.config_map['gepfile'])    
+    
+    #preProcRNA
+    preProcRNA(meta_config, study_config, '/data_{}.txt'.format(config2name_map[meta_config.alterationtype + ":" + meta_config.datahandler]), meta_config.config_map['enscon'], meta_config.config_map['genelist'], False)
+    preProcRNA(meta_config, study_config, '/data_{}_gepcomp.txt'.format(config2name_map[meta_config.alterationtype + ":" + meta_config.datahandler]), meta_config.config_map['enscon'], meta_config.config_map['genelist'], True)
 
+    # Works because shorting ...
+    if 'zscores' in meta_config.config_map.keys() and meta_config.config_map['zscores'].lower() == 'true':
+        helper.working_on(verb, message='Generating expression Z-Score Meta ...')
+        
+        #####################meta.generate_meta_type(meta_config,study_config,verb)
+        #####################meta.generate_meta_type(meta_config.alterationtype + '_ZSCORES',
+        ##################                {'profile_name': 'mRNA expression z-scores','profile_description': 'Expression level z-scores'}, study_config, verb)
+        ###################helper.working_on(verb)
+
+        helper.working_on(verb, message='Generating expression Z-Score Data ...')
+        generate_expression_zscore(meta_config, os.path.join(study_config.config_map['output_folder'],
+            'new_{}.txt'.format(config2name_map[meta_config.alterationtype + ":" + meta_config.datahandler])),
+            study_config.config_map['output_folder']
+            , False, verb)
+        helper.working_on(verb)
+
+        helper.working_on(verb, message='Generating expression Z-Score Data ...')
+        generate_expression_zscore(meta_config, os.path.join(study_config.config_map['output_folder'],
+            'new_{}_gepcomp.txt'.format(config2name_map[meta_config.alterationtype + ":" + meta_config.datahandler])),
+            study_config.config_map['output_folder']
+            , True, verb)
+        helper.working_on(verb)
+
+    #preProcRNA(meta_config, study_config, meta_config.config_map['enscon'], meta_config.config_map['genelist'], meta_config.config_map['gepcomp', meta_config.config_map['gepfile']])
+    
+    #generate_expression_zscore(os.path.join(study_config.config_map['output_folder'],
+    #    'data_{}_gepcomp.txt'.format(config2name_map[meta_config.alterationtype + ':' + meta_config.datahandler])),
+    #        os.path.join(study_config.config_map['output_folder'],
+    #            'new_{}.txt'.format(config2name_map[meta_config.alterationtype + ":Z-SCORE"]))
+    #        , verb)
+
+    generate_TCGA_data(meta_config, study_config)
 if __name__ == '__main__':
     main()
--- src/lib/analysis_pipelines/MRNA_EXPRESSION/support_functions.py
+++ src/lib/analysis_pipelines/MRNA_EXPRESSION/support_functions.py
@@ -8,24 +8,61 @@ import typing
 import pandas as pd
 import numpy as np
 import scipy as sp
+import rdflib
+import subprocess
 
 from lib.support import Config, helper
 from lib.constants.constants import config2name_map
 from numpy import *
 from scipy.stats import norm
+from rdflib.graph import Graph
 
 DataFrames = typing.List[pd.DataFrame]
 
-def preProcRNA(meta_config: Config.Config, study_config: Config.Config, enscon, genelist):
+def preProcRNA(meta_config: Config.Config, study_config: Config.Config, datafile, enscon, genelist, gepcomp):
     # read in data
     outputPath = study_config.config_map['output_folder']
-    gepData = pd.read_csv(outputPath + '/data_expression_continous.txt', sep='\t')
+    gepData = pd.read_csv(outputPath + datafile, sep='\t')
     ensConv = pd.read_csv(enscon, sep='\t')
-    ensConv.to_csv(outputPath + '/ensConv.txt', sep='\t', index=False)
 
     # rename columns
     ensConv.columns = ['gene_id', 'Hugo_Symbol']
     gepData.rename(columns={'Hugo_Symbol':'gene_id'}, inplace=True)
+
+    # merge in Hugo's
+    df = pd.merge(gepData, ensConv, on = 'gene_id', how = 'left')
+
+    # re-order columns
+    newColumns = df.columns.tolist()
+    newColumns = newColumns[-1:] + newColumns[1:-1]
+    df = df[newColumns]
+
+    newColumns = df.columns[0]
+    df = df.drop_duplicates(subset = newColumns)
+
+    # subset with given genelist
+    keep_genes_file = open(genelist, 'r+')
+    keep_genes = [line.rstrip('\n') for line in keep_genes_file.readlines()]
+    keep_genes_file.close()
+    df = df[df.Hugo_Symbol.isin(keep_genes)]
+   
+    if gepcomp:
+        df.to_csv(outputPath + '/new_{}_gepcomp.txt'.format(config2name_map[meta_config.alterationtype + ":" + meta_config.datahandler]), sep="\t", index=False)
+
+    else:
+        df.to_csv(outputPath + '/new_{}.txt'.format(config2name_map[meta_config.alterationtype + ":" + meta_config.datahandler]), sep="\t", index=False)
+
+def generate_TCGA_data(meta_config: Config.Config, study_config: Config.Config):
+    
+    ##preProcRNA(meta_config, study_config read in data
+    outputPath = study_config.config_map['output_folder']
+    #gepData = pd.read_csv(outputPath + '/data_expression_continous.txt', sep='\t')
+    #ensConv = pd.read_csv(enscon, sep='\t')
+    #ensConv.to_csv(outputPath + '/ensConv.txt', sep='\t', index=False)
+
+    # rename columns
+    #ensConv.columns = ['gene_id', 'Hugo_Symbol']
+    #gepData.rename(columns={'Hugo_Symbol':'gene_id'}, inplace=True)
 
     #ensConv_colnames = ensConv.columns
 
@@ -34,64 +71,153 @@ def preProcRNA(meta_config: Config.Config, study_config: Config.Config, enscon, 
     #ensConv.columns = ensConv_colnames
 
     # merge in Hugo's
-    df = pd.merge(gepData, ensConv, on = 'gene_id', how = 'left')
+    #df = pd.merge(gepData, ensConv, on = 'gene_id', how = 'left')
     
     # re-order columns
-    newColumns = df.columns.tolist()
-    newColumns = newColumns[-1:] + newColumns[1:-1]
-    df = df[newColumns]
+    #newColumns = df.columns.tolist()
+    #newColumns = newColumns[-1:] + newColumns[1:-1]
+    #df = df[newColumns]
     ############## TEST 3 ################
-    df.to_csv(outputPath + '/test3.txt', sep="\t", index=False)
+    #df.to_csv(outputPath + '/test3.txt', sep="\t", index=False)
     ############## TEST 3 ################
 
     # TODO deduplicate columns
-    newColumns = df.columns[0]
+    #newColumns = df.columns[0]
     #newColumns = df.columns[1:]
-    df = df.drop_duplicates(subset = newColumns)
+    #df = df.drop_duplicates(subset = newColumns)
     
     ############## TEST 2 ################
-    df.to_csv(outputPath + '/test2.txt', sep="\t", index=False)
+    #df.to_csv(outputPath + '/test2.txt', sep="\t", index=False)
     ############## TEST 2 ################
 
     # subset with given genelist
-    keep_genes_file = open(genelist, 'r+')
-    keep_genes = [line.rstrip('\n') for line in keep_genes_file.readlines()]
-    keep_genes_file.close()
-    df = df[df.Hugo_Symbol.isin(keep_genes)]
+    #keep_genes_file = open(genelist, 'r+')
+    #keep_genes = [line.rstrip('\n') for line in keep_genes_file.readlines()]
+    #keep_genes_file.close()
+    #df = df[df.Hugo_Symbol.isin(keep_genes)]
     
-    df.to_csv(outputPath + '/new_expression.txt', sep="\t", index=False)
+    #df.to_csv(outputPath + '/new_expression.txt', sep="\t", index=False)
 
     #generating zscores
-    raw_scores = df.drop(['Hugo_Symbol'], axis=1)
+    #raw_scores = df.drop(['Hugo_Symbol'], axis=1)
+    #means = raw_scores.mean(axis=1)
+    #sds = raw_scores.std(axis=1)
+
+    #z_scores = ((raw_scores.transpose() - means) / sds).transpose()
+    #z_scores = z_scores.fillna(0)
+    #z_scores = z_scores.round(decimals=4)
+    #z_scores_data = pd.concat([df['Hugo_Symbol'], z_scores], axis=1)
+
+    #z_scores_data.to_csv(outputPath + '/new_zscores.txt', sep="\t", index=False)
+
+    # Percentile STUDY
+    #newColumns = z_scores_data.columns
+    #z_scores_data[newColumns[1:]] = z_scores_data[newColumns[1:]].astype(float)
+    #newColumns = z_scores_data.columns
+    
+    # Getting ECD - using scipy
+    #z_scores_data[newColumns[1:]] = z_scores_data[newColumns[1:]].apply(lambda x: norm.cdf(x))
+    #z_scores_data[newColumns[1:]] = z_scores_data[newColumns[1:]].round(4)
+
+    # Testing ECDF
+    #z_scores_data.to_csv(outputPath + '/probability_zscores.txt', sep="\t", index=False)
+
+    # get TCGA comparitor
+    tcga_path = meta_config.config_map['tcgadata'] + '/' + meta_config.config_map['tcgacode'] + ".PANCAN.matrix.rdf"
+    #df_tcga = pd.read_csv(tcga_path, sep='')
+    #print(df_tcga.columns)
+
+    # unzip the file
+    #with open(tcga_path, "rb") as fd:
+    #    gzip_fd = gzip.GzipFile(tcga_path, 'rb')
+    #    g = Graph()
+    #    result = g.parse(gzip_fd, format='nt')
+
+    # Using rdflib
+    #g = Graph()
+    #g.parse(tcga_path, format = 'nt')
+    #print('###############')
+    #print('###############')
+    #print('###############')
+    
+    #print(len(g))
+    #print('###############')
+    #print('###############')
+    #print('###############')
+
+    # R alternative
+    command = '/.mounts/labs/gsi/modulator/sw/Ubuntu18.04/rstats-3.6/bin/Rscript'
+    path2script = '/.mounts/labs/gsiprojects/gsi/cBioGSI/aliang/cbioportal_tools/src/lib/analysis_pipelines/MRNA_EXPRESSION/get_tcga.r'
+    args = [tcga_path, meta_config.config_map['tcgacode'], outputPath]
+    cmd = [command, path2script] + args
+
+    subprocess.call(cmd)
+
+    #equalize samples
+    tcga_comp = pd.read_csv(outputPath + '/tcga_temp.txt', sep='\t')
+    df = pd.read_csv(outputPath + '/new_expression_continous.txt', sep='\t')
+    #intersected_Hugo_Symbols = pd.merge(df, tcga_comp, how = 'inner', on = ['Hugo_Symbol'])
+    #intersected_Hugo_Symbols = df.loc[df['Hugo_Symbol'].isin(tcga_comp['Hugo_Symbol'])]
+    #intersected_Hugo_Symbols = df[df['Hugo_Symbol'].isin(tcga_comp['Hugo_Symbol'])]
+    
+    #intersected_Hugo_Symbols = tcga_comp.Hugo_Symbol.index.intersection(df.Hugo_Symbol.index)
+    
+    #intersected_Hugo_Symbols = intersected_Hugo_Symbols.index.values
+    #print(type(intersected_Hugo_Symbols))
+    #intersected_Hugo_Symbols.to_csv(outputPath + '/Hugo_Symbols.txt', sep="\t", index=False)
+    
+    intersected_Hugo_Symbols = set(tcga_comp['Hugo_Symbol']).intersection(df['Hugo_Symbol'].tolist())
+    #print(intersected_Hugo_Symbols)
+    #intersected_Hugo_Symbol.to_csv(outputPath + '/new_tcga.txt', sep="\t", index=False)
+    #print(type(intersected_Hugo_Symbols))
+
+    df_tcga_common = tcga_comp[tcga_comp.Hugo_Symbol.isin(intersected_Hugo_Symbols)]
+    df_stud_common = df[df.Hugo_Symbol.isin(intersected_Hugo_Symbols)]
+    
+    # Reindex TCGA dataframe
+    df_tcga_common.reindex(df_stud_common['Hugo_Symbol'].tolist())
+
+    # TESTING #
+    #df_tcga_common.to_csv(outputPath + '/new_tcga2.txt', sep="\t", index=False)
+    #df_stud_common.to_csv(outputPath + '/new_tcga3.txt', sep="\t", index=False)
+
+    # merge samples
+    df_stud_tcga = pd.merge(df_stud_common, df_tcga_common, on = 'Hugo_Symbol', how = 'inner')
+    #df_stud_tcga = pd.concat([df_tcga_common, df_stud_common], axis = 1)
+    #df_tcga_common = df_tcga_common.loc[:, df_tcga_common.columns.intersection(df_stud_common.columns.tolist())]
+    #df_tcga_stud = df_tcga_common[df_stud_common.columns.tolist()]
+    
+    # TESTING #
+    #df_stud_tcga.to_csv(outputPath + '/new_tcga_expression.txt', sep="\t", index=False)
+    df_temp = df_stud_tcga[df_stud_common.columns.tolist()]
+    #df_temp.to_csv(outputPath + '/new_tcga_expression2.txt', sep="\t", index=False)
+    
+    #generating zscores
+    raw_scores = df_stud_tcga.drop(['Hugo_Symbol'], axis=1)
     means = raw_scores.mean(axis=1)
     sds = raw_scores.std(axis=1)
 
     z_scores = ((raw_scores.transpose() - means) / sds).transpose()
     z_scores = z_scores.fillna(0)
     z_scores = z_scores.round(decimals=4)
-    z_scores_data = pd.concat([df['Hugo_Symbol'], z_scores], axis=1)
-
-    z_scores_data.to_csv(outputPath + '/new_zscores.txt', sep="\t", index=False)
+    z_scores_data = pd.concat([df_stud_tcga['Hugo_Symbol'], z_scores], axis=1)
+    z_scores_data.to_csv(outputPath + '/data_expression_zscores_tcga2.txt', sep="\t", index=False)
+    
+    z_scores_data = z_scores_data[df_stud_common.columns.tolist()]
+    z_scores_data.to_csv(outputPath + '/data_expression_zscores_tcga.txt', sep="\t", index=False)
 
     # Percentile STUDY
     newColumns = z_scores_data.columns
     z_scores_data[newColumns[1:]] = z_scores_data[newColumns[1:]].astype(float)
     newColumns = z_scores_data.columns
-    
+
     # Getting ECD - using scipy
     z_scores_data[newColumns[1:]] = z_scores_data[newColumns[1:]].apply(lambda x: norm.cdf(x))
     z_scores_data[newColumns[1:]] = z_scores_data[newColumns[1:]].round(4)
-
-    # Testing ECDF
-    z_scores_data.to_csv(outputPath + '/probability_zscores.txt', sep="\t", index=False)
-
-    # get TCGA comparitor
-    tcga_path = meta_config.config_map['tcgadata'] + '/' + meta_config.config_map['tcgacode'] + ".PANCAN.matrix.rdf"
-    #df_tcga = pd.read_csv(tcga_path, sep='')
-    #print(df_tcga.columns)
     
-    # equalize and merge dfs (get common genes)
-    #df_stud_tcga = pd.merge(df, df_tcga, how = 'inner', on = )
+    # Testing ECDF
+    z_scores_data.to_csv(outputPath + '/data_expression_percentile_tcga.txt', sep="\t", index=False)
+
 
 def get_metadata(meta_config: Config.Config, study_config: Config.Config):
     outputPath = study_config.config_map['output_folder']
@@ -173,15 +299,53 @@ def generate_expression_matrix(exports_config: Config.Config, study_config: Conf
     result.replace(np.nan, 0, inplace=True)
 
     helper.working_on(verb, message='Writing all FPKM data ...')
+    
     result.to_csv(output_file, sep='\t', index=None)
 
+    #Append the gepcomp datafiles - Should just add columns?
+    if os.path.exists(exports_config.config_map['gepfile']):
+        geplist = pd.read_csv(exports_config.config_map['gepfile'], sep=',')
+        geplist.columns = ['patient_id', 'file_name']
+ 
+        # Filter out the patient ID's that have already been included in the study
+        indices = []
+        for i in range(exports_config.data_frame.shape[0]):
+            for a, elem in enumerate(geplist.patient_id.tolist()):
+                if exports_config.data_frame['PATIENT_ID'][i] in elem:
+                    indices.append(a)
+        geplist = geplist.drop(indices)
 
-def generate_expression_zscore(exports_config: Config.Config, study_config: Config.Config, verb):
-    input_file = os.path.join(study_config.config_map['output_folder'],
-                              'data_{}.txt'.format(config2name_map[exports_config.alterationtype + ":" + exports_config.datahandler]))
+        for index, row in geplist.iterrows():
+            info.append(pd.read_csv(row.file_name,
+                                sep='\t',
+                                usecols=['gene_id','FPKM'])
+            .rename(columns={'FPKM': row.patient_id,
+                                     'gene_id': 'Hugo_Symbol'})
+            .drop_duplicates(subset='Hugo_Symbol', keep='last', inplace=False))
 
-    output_file = os.path.join(study_config.config_map['output_folder'],
-                               'data_{}.txt'.format(config2name_map[exports_config.alterationtype + ":Z-SCORE"]))
+
+        helper.working_on(verb, message='Merging all FPKM data ...')
+        if len(info) == 0:
+            raise ImportError('Attempting to import zero expression data, please remove expression data from study.')
+        elif len(info) == 1:
+            result = info[0]
+        else:
+            result = info[0]
+            for i in range(1, len(info)):
+                result: pd.DataFrame = pd.merge(result, info[i], how='left', on='Hugo_Symbol')
+                result.drop_duplicates(subset='Hugo_Symbol', keep='last', inplace=True)
+        result.replace(np.nan, 0, inplace=True)
+
+        helper.working_on(verb, message='Writing all FPKM data ...')
+    
+        # Output the gepcomp data
+        output_file_comp = os.path.join(study_config.config_map['output_folder'], 
+                'data_{}_gepcomp.txt'.format(config2name_map[exports_config.alterationtype + ":" + exports_config.datahandler]))
+        result.to_csv(output_file_comp, sep='\t', index=None)
+
+def generate_expression_zscore(meta_config: Config.Config, input_file, outputPath, gepcomp, verb):
+    zscoreFile = '/data_{}.txt'.format(config2name_map[meta_config.alterationtype + ":Z-SCORE"])
+    percentileFile = '/data_expression_percentile.txt'
 
     # Z-Scores written by Dr. L Heisler
     helper.working_on(verb, message='Reading FPKM Matrix ...')
@@ -198,4 +362,29 @@ def generate_expression_zscore(exports_config: Config.Config, study_config: Conf
     z_scores_data = pd.concat([raw_data['Hugo_Symbol'], z_scores], axis=1)
 
     helper.working_on(verb, message='Writing FPKM Z-Scores Matrix ...')
-    z_scores_data.to_csv(output_file, sep="\t", index=False)
+    
+    # Percentile STUDY
+    newColumns = z_scores_data.columns
+    percentile_data = z_scores_data[newColumns[1:]].astype(float)
+    newColumns = percentile_data.columns
+
+    # Getting ECD - using scipy
+    percentile_data = percentile_data.apply(lambda x: norm.cdf(x))
+    percentile_data = percentile_data.round(4)
+    percentile_data = pd.concat([raw_data['Hugo_Symbol'], percentile_data], axis=1)
+
+    if gepcomp:
+        study_columns = []
+        for k in range(meta_config.data_frame.shape[0]):
+            study_columns.append(meta_config.data_frame['SAMPLE_ID'][k])
+        study_columns.insert(0,'Hugo_Symbol')
+        z_scores_data = z_scores_data[study_columns]
+        percentile_data = percentile_data[study_columns]
+        zscoreFile = '/data_expression_zscores_comparison.txt'
+        percentileFile = '/data_expression_percentile_comparison.txt'
+    
+    # Output Z scores
+    z_scores_data.to_csv(outputPath + zscoreFile, sep="\t", index=False)
+
+    # Output ECDF
+    percentile_data.to_csv(outputPath + percentileFile, sep="\t", index=False)
