f0a3eb844fb40e1aa9fe708d01f9bbeb6002e972
Allan Liang <aliang@oicr.on.ca>
2020-04-07 17:48:47 -0400

    delete uneeded comments

--- src/lib/analysis_pipelines/MRNA_EXPRESSION/support_functions.py
+++ src/lib/analysis_pipelines/MRNA_EXPRESSION/support_functions.py
@@ -53,98 +53,11 @@ def preProcRNA(meta_config: Config.Config, study_config: Config.Config, datafile
         df.to_csv(outputPath + '/new_{}.txt'.format(config2name_map[meta_config.alterationtype + ":" + meta_config.datahandler]), sep="\t", index=False)
 
 def generate_TCGA_data(meta_config: Config.Config, study_config: Config.Config):
-    
-    ##preProcRNA(meta_config, study_config read in data
     outputPath = study_config.config_map['output_folder']
-    #gepData = pd.read_csv(outputPath + '/data_expression_continous.txt', sep='\t')
-    #ensConv = pd.read_csv(enscon, sep='\t')
-    #ensConv.to_csv(outputPath + '/ensConv.txt', sep='\t', index=False)
-
-    # rename columns
-    #ensConv.columns = ['gene_id', 'Hugo_Symbol']
-    #gepData.rename(columns={'Hugo_Symbol':'gene_id'}, inplace=True)
-
-    #ensConv_colnames = ensConv.columns
-
-    #ensConv_colnames[0] = 'gene_id'
-    #ensConv_colnames[1] = 'Hugo_Symbol'
-    #ensConv.columns = ensConv_colnames
-
-    # merge in Hugo's
-    #df = pd.merge(gepData, ensConv, on = 'gene_id', how = 'left')
-    
-    # re-order columns
-    #newColumns = df.columns.tolist()
-    #newColumns = newColumns[-1:] + newColumns[1:-1]
-    #df = df[newColumns]
-    ############## TEST 3 ################
-    #df.to_csv(outputPath + '/test3.txt', sep="\t", index=False)
-    ############## TEST 3 ################
-
-    # TODO deduplicate columns
-    #newColumns = df.columns[0]
-    #newColumns = df.columns[1:]
-    #df = df.drop_duplicates(subset = newColumns)
-    
-    ############## TEST 2 ################
-    #df.to_csv(outputPath + '/test2.txt', sep="\t", index=False)
-    ############## TEST 2 ################
-
-    # subset with given genelist
-    #keep_genes_file = open(genelist, 'r+')
-    #keep_genes = [line.rstrip('\n') for line in keep_genes_file.readlines()]
-    #keep_genes_file.close()
-    #df = df[df.Hugo_Symbol.isin(keep_genes)]
-    
-    #df.to_csv(outputPath + '/new_expression.txt', sep="\t", index=False)
-
-    #generating zscores
-    #raw_scores = df.drop(['Hugo_Symbol'], axis=1)
-    #means = raw_scores.mean(axis=1)
-    #sds = raw_scores.std(axis=1)
-
-    #z_scores = ((raw_scores.transpose() - means) / sds).transpose()
-    #z_scores = z_scores.fillna(0)
-    #z_scores = z_scores.round(decimals=4)
-    #z_scores_data = pd.concat([df['Hugo_Symbol'], z_scores], axis=1)
-
-    #z_scores_data.to_csv(outputPath + '/new_zscores.txt', sep="\t", index=False)
-
-    # Percentile STUDY
-    #newColumns = z_scores_data.columns
-    #z_scores_data[newColumns[1:]] = z_scores_data[newColumns[1:]].astype(float)
-    #newColumns = z_scores_data.columns
-    
-    # Getting ECD - using scipy
-    #z_scores_data[newColumns[1:]] = z_scores_data[newColumns[1:]].apply(lambda x: norm.cdf(x))
-    #z_scores_data[newColumns[1:]] = z_scores_data[newColumns[1:]].round(4)
-
-    # Testing ECDF
-    #z_scores_data.to_csv(outputPath + '/probability_zscores.txt', sep="\t", index=False)
 
     # get TCGA comparitor
     tcga_path = meta_config.config_map['tcgadata'] + '/' + meta_config.config_map['tcgacode'] + ".PANCAN.matrix.rdf"
-    #df_tcga = pd.read_csv(tcga_path, sep='')
-    #print(df_tcga.columns)
-
-    # unzip the file
-    #with open(tcga_path, "rb") as fd:
-    #    gzip_fd = gzip.GzipFile(tcga_path, 'rb')
-    #    g = Graph()
-    #    result = g.parse(gzip_fd, format='nt')
-
-    # Using rdflib
-    #g = Graph()
-    #g.parse(tcga_path, format = 'nt')
-    #print('###############')
-    #print('###############')
-    #print('###############')
     
-    #print(len(g))
-    #print('###############')
-    #print('###############')
-    #print('###############')
-
     # R alternative
     command = '/.mounts/labs/gsi/modulator/sw/Ubuntu18.04/rstats-3.6/bin/Rscript'
     path2script = '/.mounts/labs/gsiprojects/gsi/cBioGSI/aliang/cbioportal_tools/src/lib/analysis_pipelines/MRNA_EXPRESSION/get_tcga.r'
@@ -156,20 +69,8 @@ def generate_TCGA_data(meta_config: Config.Config, study_config: Config.Config):
     #equalize samples
     tcga_comp = pd.read_csv(outputPath + '/tcga_temp.txt', sep='\t')
     df = pd.read_csv(outputPath + '/new_expression_continous.txt', sep='\t')
-    #intersected_Hugo_Symbols = pd.merge(df, tcga_comp, how = 'inner', on = ['Hugo_Symbol'])
-    #intersected_Hugo_Symbols = df.loc[df['Hugo_Symbol'].isin(tcga_comp['Hugo_Symbol'])]
-    #intersected_Hugo_Symbols = df[df['Hugo_Symbol'].isin(tcga_comp['Hugo_Symbol'])]
-    
-    #intersected_Hugo_Symbols = tcga_comp.Hugo_Symbol.index.intersection(df.Hugo_Symbol.index)
-    
-    #intersected_Hugo_Symbols = intersected_Hugo_Symbols.index.values
-    #print(type(intersected_Hugo_Symbols))
-    #intersected_Hugo_Symbols.to_csv(outputPath + '/Hugo_Symbols.txt', sep="\t", index=False)
-    
+     
     intersected_Hugo_Symbols = set(tcga_comp['Hugo_Symbol']).intersection(df['Hugo_Symbol'].tolist())
-    #print(intersected_Hugo_Symbols)
-    #intersected_Hugo_Symbol.to_csv(outputPath + '/new_tcga.txt', sep="\t", index=False)
-    #print(type(intersected_Hugo_Symbols))
 
     df_tcga_common = tcga_comp[tcga_comp.Hugo_Symbol.isin(intersected_Hugo_Symbols)]
     df_stud_common = df[df.Hugo_Symbol.isin(intersected_Hugo_Symbols)]
@@ -177,21 +78,11 @@ def generate_TCGA_data(meta_config: Config.Config, study_config: Config.Config):
     # Reindex TCGA dataframe
     df_tcga_common.reindex(df_stud_common['Hugo_Symbol'].tolist())
 
-    # TESTING #
-    #df_tcga_common.to_csv(outputPath + '/new_tcga2.txt', sep="\t", index=False)
-    #df_stud_common.to_csv(outputPath + '/new_tcga3.txt', sep="\t", index=False)
-
     # merge samples
     df_stud_tcga = pd.merge(df_stud_common, df_tcga_common, on = 'Hugo_Symbol', how = 'inner')
-    #df_stud_tcga = pd.concat([df_tcga_common, df_stud_common], axis = 1)
-    #df_tcga_common = df_tcga_common.loc[:, df_tcga_common.columns.intersection(df_stud_common.columns.tolist())]
-    #df_tcga_stud = df_tcga_common[df_stud_common.columns.tolist()]
     
-    # TESTING #
-    #df_stud_tcga.to_csv(outputPath + '/new_tcga_expression.txt', sep="\t", index=False)
     df_temp = df_stud_tcga[df_stud_common.columns.tolist()]
-    #df_temp.to_csv(outputPath + '/new_tcga_expression2.txt', sep="\t", index=False)
-    
+     
     #generating zscores
     raw_scores = df_stud_tcga.drop(['Hugo_Symbol'], axis=1)
     means = raw_scores.mean(axis=1)
