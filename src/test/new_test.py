a797a7a47be4db2c970872486018185e62a7eb2b
iainrb <1695061+iainrb@users.noreply.github.com>
2020-07-24 15:23:06 -0400

    Clean up tests, reduce duplication of code

--- src/test/new_test.py
+++ src/test/new_test.py
@@ -4,31 +4,18 @@ import hashlib, logging, os, tempfile, unittest
 
 from generate.study import study
 
-# TODO refactor live/dry-run tests to reduce duplication of code
-
 class TestStudy(unittest.TestCase):
 
+    """Tests for Janus study generation"""
+
     def setUp(self):
         self.testDir = os.path.dirname(os.path.realpath(__file__))
         self.dataDir = os.path.realpath(
             os.path.join(self.testDir, os.pardir, os.pardir, 'study_input', 'examples_new')
         )
         self.tmp = tempfile.TemporaryDirectory(prefix='janus_study_test_')
-        #self.outDir = os.path.join(self.tmp.name, 'CAP_expression_test')
-        #os.mkdir(self.outDir)
-        ### temporary setup for output directory -- won't be deleted on test exit
-        self.outDir = os.path.join('/tmp', 'CAP_expression_test')
-        from shutil import rmtree
-        if os.path.exists(self.outDir):
-            rmtree(self.outDir)
-        os.mkdir(self.outDir)
-        ###
-        config_path = os.path.join(self.dataDir, 'CAP_expression', 'study.txt')
-        test_study = study(config_path, log_level=logging.DEBUG)
-        test_study.write_all(self.outDir, dry_run=False)
-
-    def test_file_output(self):
-        checksums = {
+        self.config_path = os.path.join(self.dataDir, 'CAP_expression', 'study.txt')
+        self.base_checksums = {
             'data_cancer_type.txt': 'c23f3a55d260022616a5a95b837c49d0',
             'data_clinical_patients.txt': '852777b8f1bc60b134c9dc999ac87a24',
             'data_clinical_samples.txt': '199053f38a24c52072418a42dba3fdf4',
@@ -38,54 +25,34 @@ class TestStudy(unittest.TestCase):
             'meta_study.txt': '5ca90314306ad1f1aae94bc345bd0a23',
             'case_lists/cases_merp.txt': '43685fab767e5961a11e68a45d68c5ec'
         }
-        for name in checksums.keys():
-            outPath = os.path.join(self.outDir, name)
-            self.assertTrue(os.path.exists(outPath), outPath+" exists")
-            md5 = hashlib.md5()
-            with open(outPath, 'rb') as f:
-                md5.update(f.read())
-            self.assertEqual(md5.hexdigest(),
-                             checksums[name],
-                             outPath+" checksums match")
 
-    def tearDown(self):
-        self.tmp.cleanup()
+    def test_dry_run(self):
+        outDir = self.outDir = os.path.join(self.tmp.name, 'dry_run')
+        os.mkdir(self.outdir)
+        test_study = study(self.config_path, log_level=logging.DEBUG)
+        test_study.write_all(outDir, dry_run=True)
+        self.verify_checksums(self.base_checksums)
 
-class TestStudyDryRun(unittest.TestCase):
-
-    def setUp(self):
-        self.testDir = os.path.dirname(os.path.realpath(__file__))
-        self.dataDir = os.path.realpath(
-            os.path.join(self.testDir, os.pardir, os.pardir, 'study_input', 'examples_new')
-        )
-        self.tmp = tempfile.TemporaryDirectory(prefix='janus_study_test_')
-        #self.outDir = os.path.join(self.tmp.name, 'CAP_expression_test')
-        #os.mkdir(self.outDir)
-        ### temporary setup for output directory -- won't be deleted on test exit
-        self.outDir = os.path.join('/tmp', 'CAP_expression_test_dryrun')
-        from shutil import rmtree
-        if os.path.exists(self.outDir):
-            rmtree(self.outDir)
-        os.mkdir(self.outDir)
-        ###
-        config_path = os.path.join(self.dataDir, 'CAP_expression', 'study.txt')
-        test_study = study(config_path, log_level=logging.DEBUG)
-        test_study.write_all(self.outDir, dry_run=True)
-
-    def test_file_output(self):
-        checksums = {
-            'data_cancer_type.txt': 'c23f3a55d260022616a5a95b837c49d0',
-            'data_clinical_patients.txt': '852777b8f1bc60b134c9dc999ac87a24',
-            'data_clinical_samples.txt': '199053f38a24c52072418a42dba3fdf4',
-            'meta_cancer_type.txt': '19d950648288bb7428e8aaf5ee2939a0',
-            'meta_clinical_patients.txt': '0de6a7ae349e16b26b68ac5a4eb62a0c',
-            'meta_clinical_samples.txt': '42609db9577d6192113be9ffeba92292',
-            'meta_study.txt': '5ca90314306ad1f1aae94bc345bd0a23',
-            'case_lists/cases_merp.txt': '43685fab767e5961a11e68a45d68c5ec'
+    def test_CAP_expression(self):
+        outDir = self.outDir = os.path.join(self.tmp.name, 'CAP_expression')
+        os.mkdir(self.outdir)
+        test_study = study(self.config_path, log_level=logging.DEBUG)
+        test_study.write_all(outDir, dry_run=False)
+        more_checksums = {
+            'data_expression_continous.txt', 'e2b50dc44307e0b9bee27d253b02c6d9',
+            'data_expression_zscores.txt', '0a04f5f68265ca9a1aded16dd013738c',
+            'meta_expression_continous.txt', '5db83d4ca1925117abc8837b2eebeb46',
+            'meta_expression_zscores.txt', '4c807196b4d1e1e47710bd96343b3ccc',
         }
-        for name in checksums.keys():
-            outPath = os.path.join(self.outDir, name)
-            self.assertTrue(os.path.exists(outPath), outPath+" exists")
+        checksums = self.base_checksums.copy()
+        checksums.update(more_checksums)
+        self.verify_checksums(checksums)
+
+    def verify_checksums(self, checksums):
+        """Checksums is a dictionary: md5sum -> relative path from output directory """
+         for relative_path in checksums.keys():
+            outPath = os.path.join(self.outDir, relative_path)
+            self.assertTrue(os.path.exists(outPath), rel+" exists")
             md5 = hashlib.md5()
             with open(outPath, 'rb') as f:
                 md5.update(f.read())
