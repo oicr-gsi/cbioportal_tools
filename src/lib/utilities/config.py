d0c8bea58f15065e5c8c93f007ecb03cf8c70db8
iainrb <1695061+iainrb@users.noreply.github.com>
2020-08-27 15:51:59 -0400

    First pass at adding schema validation, with tests

--- src/lib/utilities/config.py
+++ src/lib/utilities/config.py
@@ -8,6 +8,8 @@ import re
 import os
 import yaml
 
+import sys # TODO FIXME
+
 import utilities.constants
 from utilities.base import base
 
@@ -25,10 +27,14 @@ class config(base):
     REQUIRED_META_FIELDS = []
     OPTIONAL_META_FIELDS = []
     
-    def __init__(self, input_path, log_level=logging.WARNING, name=None, strict=False):
+    def __init__(self, input_path, schema_path=None, log_level=logging.WARNING, name=None, strict=False):
         if name == None:
             name = "%s.%s"% (__name__, type(self).__name__)
         self.logger = self.get_logger(log_level, name)
+        if schema_path != None:
+            self.schema = schema(schema_path, log_level=log_level)
+        else:
+            self.schema = None
         self.config_dir = os.path.abspath(os.path.dirname(input_path))
         [self.meta, skip_total] = self.read_meta(input_path)
         if strict:
@@ -38,6 +44,16 @@ class config(base):
     def data_as_tsv(self):
         return self.table.to_csv(sep="\t", index=False)
 
+    def contents_equal(self, other):
+        """Check if contents equal with another config"""
+        try:
+            equal = self.meta == other.get_meta() and self.table.equals(other.get_table())
+        except AttributeError as err:
+            msg = "Attribute not found. Attempting to compare with a non-config object? "+str(err)
+            self.logger.error(msg)
+            raise
+        return equal
+    
     def get_meta(self):
         return self.meta
 
@@ -81,24 +97,19 @@ class config(base):
         meta = yaml.safe_load(''.join(yaml_lines))
         return (meta, skip_rows)
 
-    def validate_meta_fields(self):
-        missing_required = []
-        for field in self.REQUIRED_META:
-            if not self.meta.has_key(field):
-                missing_required.append(field)
-        expected_fields = set()
-        expected_fields.update(self.REQUIRED_META)
-        expected_fields.update(self.OPTIONAL_META)
-        unexpected = []
-        for field in self.meta.keys():
-            if field not in self.expected_fields():
-                unexpected.append(field)
-        if len(unexpected) > 0:
-            self.logger.warn('Unexpected metadata fields found: '+', '.join(unexpected))
-        if len(missing_required) > 0:
-            msg = 'Missing required metadata fields: '+', '.join(missing_required)
-            self.logger.error(msg)
-            raise JanusConfigError(msg)
+    def validate_syntax(self):
+        """Validate header and body syntax against the schema"""
+        if self.schema == None:
+            raise JanusConfigError("Cannot validate syntax because no schema is specified")
+        meta_valid = self.schema.validate_meta(self.meta)
+        body_valid = self.schema.validate_table(self.table)
+        return meta_valid and body_valid
+
+    def validate_meta_paths(self):
+        """Validate paths in the header"""
+        if self.schema == None:
+            raise JanusConfigError("Cannot validate syntax because no schema is specified")
+        return self.schema.validate_meta_paths(self.header)
 
 
 class legacy_config_wrapper(base):
@@ -125,6 +136,115 @@ class legacy_config_wrapper(base):
 
     def __str__(self):
         return str([self.config_map, self.data_frame, self.datahandler, self.alterationtype])
+
+class schema(base):
+
+    """Schema class for a Janus config file, with validation/template methods"""
+
+    def __init__(self, schema_path, log_level=logging.WARNING):
+        name = "%s.%s"% (__name__, type(self).__name__)
+        self.logger = self.get_logger(log_level, name)
+        with open(schema_path, 'r') as schema_file:
+            schema = yaml.safe_load(schema_file.read())
+        self.head = schema.get('head')
+        if not self.head:
+            msg = "No 'head' entry in Janus schema path '%s'" % schema_path
+            self.logger.error(msg)
+            raise JanusConfigError(msg)
+        self.body = schema.get('body')
+        if not self.body:
+            msg = "No 'body' entry in Janus schema path '%s'" % schema_path
+            self.logger.error(msg)
+            raise JanusConfigError(msg)
+        self.all_head_keys = set(self.head.keys())
+        self.required_head_keys = set()
+        for key in self.head.keys():
+            if self.head[key].get('required'):
+                self.required_head_keys.add(key)
+
+    def _generate_header_template(self, schema_dict):
+        """
+        Change a schema dictionary into a Janus config header template.
+        Use recursively to generate templates for dictionaries in the header.
+        """
+        template = {}
+        for key in schema_dict.keys():
+            schema_val = schema_dict.get(key)
+            type_string = schema_val.get('type', 'any')
+            if type_string == 'dictionary':
+                template_val = self._generate_header_template(schema_val['contents'])
+            elif schema_val.get('required'):
+                template_val = '%s: REQUIRED' % type_string
+            else:
+                template_val = '%s: OPTIONAL' % type_string
+            template[key] = template_val
+        return template
+
+    def validate_table(self, table):
+        """validate a Pandas dataframe against the schema"""
+        valid = True
+        table_column_names = list(table.columns.values)
+        if table_column_names != self.body:
+            msg = "Input columns [%s] do not match schema columns [%s]" % (
+                ', '.join(table_column_names),
+                ', '.join(self.body),
+            )
+            self.logger.warning(msg)
+            valid = False
+        return valid
+            
+    def validate_meta(self, meta):
+        """validate a header dictionary against the schema"""
+        valid = True
+        meta_key_set = set(meta.keys())
+        missing_set = self.required_head_keys - meta_key_set
+        if len(missing_set) > 0:
+            missing = ', '.join(sorted(list(missing_set)))
+            self.logger.warning("Missing required header fields: %s" % missing)
+            valid = False
+        extra_set = meta_key_set - self.all_head_keys
+        if len(extra_set) > 0:
+            extra = ', '.join(sorted(list(extra_set)))
+            self.logger.warning("Extra fields not present in schema: %s" % extra)
+            valid = False
+        return valid
+
+    def validate_meta_paths(self, meta):
+        """
+        Check any paths in the header dictionary exist and are readable.
+        Relative paths are checked with respect to the current working directory.
+        Does not (yet) check for type (file vs. directory) or writability.
+        """
+        valid = True
+        msg = "Validating paths with respect to the current working directory %s" % os.getcwd()
+        self.logger.info(msg)
+        for key, val in meta:
+            schema_val = self.head.get(key)
+            if schema_val.get('type') == 'path':
+                if not os.path.exists(val):
+                    self.logger.warn("Path '%s' does not exist" % val)
+                    valid = False
+                elif not os.access(val, os.R_OK):
+                    self.logger.warn("Path '%s' exists but is not readable" % val)
+                    valid = False
+        if valid:
+            self.logger.info("All paths in metadata exist and are readable.")
+        else:
+            self.logger.warn("One or more path in metadata does not exist or is not readable.")
+        return valid
+
+    def write_template(self, out_file):
+        """write a template based on the schema"""
+        yaml_delimiter = '---'
+        print(yaml_delimiter, file=out_file)
+        header = self._generate_header_template(self.head)
+        # dump header to YAML and convert to list of (non-empty) strings
+        header_lines = [x for x in re.split('\n', yaml.dump(header)) if x!='']
+        for line in header_lines:
+            print('#'+line, file=out_file)
+        print(yaml_delimiter, file=out_file)
+        print('\t'.join(self.body), file=out_file)
+        print('\t'.join([x.lower()+'_value' for x in self.body]), file=out_file)
 
 
 class JanusConfigError(Exception):
