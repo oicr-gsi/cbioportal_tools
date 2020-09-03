7dfc8031affb509eb49fe084b77f3dbf343be121
iainrb <1695061+iainrb@users.noreply.github.com>
2020-09-03 16:57:48 -0400

    Changes to improve help text

--- src/bin/janus.py
+++ src/bin/janus.py
@@ -8,6 +8,11 @@ import sys
 
 from utilities.main import main
 
+GENERATE = 'Generate cBioPortal study from pipeline data. Requires '+\
+           'appropriate Janus configuration files.'
+TEMPLATE = 'Write a template Janus configuration file using a schema.'
+VALIDATE = 'Validate a Janus configuration file against a schema.'
+
 def super_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         description='janus: A cBioPortal gateway')
@@ -15,38 +20,33 @@ def super_parser() -> argparse.ArgumentParser:
     parser.add_argument('--verbose', action='store_true', help="More verbose logging")
     parser.add_argument('-l', '--log-path', metavar='PATH', help='Path of file where '+\
                         'log output will be appended. Optional, defaults to STDERR.')
-    subparsers = parser.add_subparsers(title='Janus subcommands',
-                                       dest='which')
+    subparsers = parser.add_subparsers(title='Janus subcommands', dest='which')
+
+    # 'help' appears in main help, 'description' in subparser help
     add_generation_arguments(
-        subparsers.add_parser('generate',
-                              help='Generate cBioPortal study from pipeline data. Requires '+\
-                              'appropriate Janus configuration files.')
+        subparsers.add_parser('generate', description=GENERATE, help=GENERATE)
     )
     add_template_arguments(
-        subparsers.add_parser('template',
-                              help='Write a template Janus configuration file using a schema.')
+        subparsers.add_parser('template', description=TEMPLATE, help=TEMPLATE)
     )
     add_validation_arguments(
-        subparsers.add_parser('validate',
-                              help='Validate a Janus configuration file against a schema.')
+        subparsers.add_parser('validate', description=VALIDATE, help=VALIDATE)
     )
     return parser
 
 def add_generation_arguments(parser):
-    required = parser.add_argument_group('Required arguments')
-
+    parser.add_argument("-d", "--dry-run",
+                          action="store_true",
+                          help="Dry-run mode; write sample, patient, and metadata files, but "+\
+                          "do not process pipeline data. Used for rapid testing and sanity-checking.")
+    parser.add_argument("-f", "--force",
+                          action="store_true",
+                          help="Force overwrite of output folder; delete previous contents, if any.")
+    required = parser.add_argument_group('required arguments')
     required.add_argument("-c", "--config", help="Path to study config file",
                           metavar='PATH', required=True)
     required.add_argument("-o", "--out", help="Directory for study output",
                           metavar='PATH', required=True)
-    optional = parser.add_argument_group('Optional arguments')
-    optional.add_argument("-d", "--dry-run",
-                          action="store_true",
-                          help="Dry-run mode; write sample, patient, and metadata files, but "+\
-                          "do not process pipeline data. Used for rapid testing and sanity-checking.")
-    optional.add_argument("-f", "--force",
-                          action="store_true",
-                          help="Force overwrite of output folder; delete previous contents, if any.")
 
 def add_template_arguments(parser):
     parser.add_argument("-d", "--describe",
